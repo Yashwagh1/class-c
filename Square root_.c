44d476ad0a4a5bd8b1526b92b4f74aa48d45de47
Yashwagh1 <103913520+Yashwagh1@users.noreply.github.com>
2022-04-29 20:43:16 +0530

    Add files via upload

---
+++ Square root_.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float number;
+    printf("Enter an integer number ");
+    scanf("%f", &number);
+    printf("%f\n",sqrt(number) );
+    return 0; 
+}
